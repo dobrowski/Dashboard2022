5a720de6eb27b4366ea9b42d6a8f831112e54d41
David Dobrowski <dobrowski@gmail.com>
2022-11-18 10:04:11 -0800

    Init

---
+++ 1 Setup.R
@@ -0,0 +1,43 @@
+
+
+library(here)
+library(tidyverse)
+library(MCOE)
+library(googlesheets4)
+
+
+con <- mcoe_sql_con()
+
+
+dash_census <- tbl(con, "DASH_CENSUS") %>% 
+    filter(countyname == "Monterey",
+           rtype == "D",
+           reportingyear  == 2019
+    ) %>%
+#           head(20) %>%
+    collect() 
+
+
+# districts <- c("Greenfield",
+#                "King City", 
+#                "Salinas Union",
+#                "Santa Rita"
+# )
+# 
+# districts.list <- paste0(districts, collapse = "|")
+# 
+# 
+
+groups <- dash_census %>%
+    filter(subgrouptotal >= 30)
+
+
+foster <- dash_census %>%
+    filter(studentgroup == "FOS" & subgrouptotal >= 15 & subgrouptotal <=30)
+
+
+groups <- bind_rows(groups,foster) %>%
+    select(districtname, studentgroup)
+
+
+write_sheet(groups, "https://docs.google.com/spreadsheets/d/1J6SYEaGQJrNfYKegi7HZ_NgjMvYXgFa2fCaMxZK-U7M/edit#gid=0")
